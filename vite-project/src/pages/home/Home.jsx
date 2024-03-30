0dfe5019118c6f36ec60e316d7c4f9c7e3ca905d
Priti Mondal <preeti100mondal@gmail.com>
2024-03-31 00:24:08 +0530

    save changes

--- vite-project/src/pages/home/Home.jsx
+++ vite-project/src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import  myContext  from '../../context/data/myContext';
+import HeroSection from '../../components/heroSection/HeroSection';
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard';
 
 
 function Home() {
  
   return (
     <Layout>
-     Home 
+     <HeroSection/>
+     <Filter/>
+     <ProductCard/>
     </Layout>
   )
 }
