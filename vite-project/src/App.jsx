c7dc7707268e8a19bfd6987f3e40753083c19c07
Priti Mondal <preeti100mondal@gmail.com>
2024-03-30 17:28:15 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home"
+import Cart from "./pages/cart/Cart"
+import Order from "./pages/order/Order"
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+import MyState from "./context/data/myState"
+
+function App() {
+  return (
+    <MyState>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/order" element={<Order/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/*" element={<NoPage/>}/>
+
+      </Routes>
+    </Router>
+    </MyState>
+  )
+}
+
+export default App
---
+++ vite-project/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import  myContext  from '../../context/data/myContext';
+
+
+function Home() {
+ 
+  return (
+    <Layout>
+     Home 
+    </Layout>
+  )
+}
+
+export default Home
